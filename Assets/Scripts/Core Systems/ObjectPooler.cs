db9161e3fccd3a9d8b5753ba76415f4331fe8631
AHodg010 <M_H295@student.usc.edu.au>
2025-05-29 00:44:36 +1000

    InputManager and ObjectPooler scripts

---
+++ Assets/Scripts/Core Systems/ObjectPooler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> ObjectPool = new List<GameObject>();
+    [SerializeField]
+    private GameObject prefab;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject newObject = Instantiate(prefab);
+            newObject.SetActive(false);
+
+            ObjectPool.Add(newObject);
+        }
+    }
+    public GameObject GetObjectFromPool()
+    {
+        if (ObjectPool.Count == 0)
+        {
+            return null;
+        }
+
+        GameObject obj = ObjectPool[0];
+        ObjectPool.Remove(obj);
+        return obj;
+    }
+
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        ObjectPool.Add(obj);
+    }
+}
